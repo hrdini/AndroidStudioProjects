c96eaba0744f4972e4212b427f532c21ae4ff666
hrdini <hardinifebrijati.rpl@gmail.com>
2023-01-27 19:08:55 +0700

    updated

--- RecyclerViewCardView/app/src/main/java/com/hrdini/recyclerviewcardview/MainActivity.java
+++ RecyclerViewCardView/app/src/main/java/com/hrdini/recyclerviewcardview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new SiswaAdapter(this, siswaList);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void btnTambah(View view) {
+        siswaList.add(new Siswa("Sambo", "Bojong Gede"));
     }
 }
--- RecyclerViewCardView/app/src/main/java/com/hrdini/recyclerviewcardview/SiswaAdapter.java
+++ RecyclerViewCardView/app/src/main/java/com/hrdini/recyclerviewcardview/SiswaAdapter.java
@@ -2,8 +2,10 @@ package com.hrdini.recyclerviewcardview;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,15 +32,47 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        Siswa siswa = siswaList.get(i);
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
+        final Siswa siswa = siswaList.get(i);
         viewHolder.tvNama.setText(siswa.getNama());
         viewHolder.tvAlamat.setText(siswa.getAlamat());
 
+        /*
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Nama : "+siswa.getNama()+"Alamat : "+siswa.getAlamat(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+         */
+
+        viewHolder.tvMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(context,viewHolder.tvMenu);
+                popupMenu.inflate(R.menu.menu_option);
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_simpan:
+                                Toast.makeText(context, "Simpan Data "+siswa.getNama(), Toast.LENGTH_SHORT).show();
+                                break;
+
+                                case R.id.menu_hapus:
+                                    siswaList.remove(i);
+                                    notifyDataSetChanged();
+                                    Toast.makeText(context, siswa.getNama()+"Data berhasil dihapus!", Toast.LENGTH_SHORT).show();
+                                    break;
+                        }
+
+                        return false;
+                    }
+                });
+
+                popupMenu.show();
             }
         });
     }
@@ -49,12 +83,14 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView tvNama, tvAlamat;
+        TextView tvNama, tvAlamat, tvMenu;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             tvNama = itemView.findViewById(R.id.tvNama);
             tvAlamat = itemView.findViewById(R.id.tvAlamat);
+            tvMenu = itemView.findViewById(R.id.tvMenu);
         }
     }
 }
